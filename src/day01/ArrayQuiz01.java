d2e175f616b27f542eefceaca385b68c668127fa
202216008 <apdlfdldjqtm@gmail.com>
2023-09-07 09:26:24 +0900

    # 배열 문제 풀이

--- src/day01/ArrayQuiz01.java
+++ src/day01/ArrayQuiz01.java
@@ -9,7 +9,50 @@ public class ArrayQuiz01 {
         System.out.println("# 먹고 싶은 음식을 입력하세요!!");
         System.out.println("# 입력을 중지하려면 <그만>이라고 입력하세요,");
 
-        String[] foodArray={"딸기","사과","오렌지","레몬","자몽"};
+        // 풀이
+
+        //빈 배열 만들기
+        String[] foodList = new String[0];
+//        String[] foodList = {};
+
+        /*
+            # 음식명을 입력받아서 배열에 순서대로 저장하는 코드
+
+            1. 음식명을 입력받는다.
+            2. 음식명에 '그만'이 입력되었는가?
+            2-1. 그만이 입력되었으면 종료한다.
+            2-2. 그만이 입력되지 않았으면 배열에 넣는다.
+            3. 배열의 크기를 1 증가시킨다.
+            4. 기존의 음식데이터를 증가된 새 배열에 복사한다.
+            5. 새로운 음식명을 마지막 위치에 추가한다.
+         */
+
+        System.out.println("# 먹고 싶은 음식을 입력하세요!!");
+        System.out.println("# 입력을 중지하려면 <그만>이라고 입력하세요.");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.print(">> ");
+            String newFood = sc.nextLine();
+
+
+            if (newFood.equals("그만")) break;
+
+            String[] temp = new String[foodList.length + 1];
+            for (int i = 0; i < foodList.length; i++) {
+                temp[i] = foodList[i];
+            }
+            temp[temp.length - 1] = newFood;
+            foodList = temp;
+            temp = null;
+
+        } // end while
+
+        System.out.println("먹고 싶은 음식리스트: " + Arrays.toString(foodList));
+
+        /*String[] foodArray={"딸기","사과","오렌지","레몬","자몽"};
         int index=2;
         String newData="샤인머스켓";
 
@@ -21,10 +64,7 @@ public class ArrayQuiz01 {
             foodCopy[i]=foodCopy[i+1];
         }
 
-        System.out.println("foodCopy = " + Arrays.toString(foodCopy));
-
-
-
+        System.out.println("foodCopy = " + Arrays.toString(foodCopy));*/
 
     }//end main
 }
