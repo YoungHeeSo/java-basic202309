16f21e90be62d86ed7c748b49229a9ae6c0c90a4
202216008 <apdlfdldjqtm@gmail.com>
2023-09-08 10:30:46 +0900

    # 패키지 학습

---
+++ src/day03/player/main.java
@@ -0,0 +1,17 @@
+package day03.player;
+
+public class main {
+    public static void main(String[] args) {
+
+        //기본생성자는 생성자가 없어야 자동생성 돼
+        //생성자가 하나라도 있다면 수동으로 만들어줘야 돼
+        new Player();
+
+        Player parking=new Player("주차왕파킹");
+        Player fucking=new Player("욕설왕퍼킹");
+
+        parking.attack(fucking);
+        System.out.println("parking = " + parking);
+        System.out.println("fucking = " + fucking);
+    }
+}
